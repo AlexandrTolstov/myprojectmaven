7c8238f8d9c9943fefec4d67a2d818bd37028edf
alex <tosltov.alexandr.85@gmail.com>
2025-01-14 18:23:21 +0500

    Final

---
+++ src/main/java/ru/geekbrain/App.java
@@ -0,0 +1,15 @@
+package ru.geekbrain;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+        String[] words = {"Hello", "World"};
+    }
+}
